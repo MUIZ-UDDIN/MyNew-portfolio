9526c40a6ce8d5c6da7bbd89e633f59e4cd1d84c
MUIZ UD DIN <mueezgamings@gmail.com>
2026-06-28 22:27:59 +0500

    sitemap issue fixed

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://muizuddin.dev/sitemap.xml",
+    sitemap: "https://muizuddin.vercel.app/sitemap.xml",
   }
 }
